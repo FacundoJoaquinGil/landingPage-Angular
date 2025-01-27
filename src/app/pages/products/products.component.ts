241fae4afb351e2696538f155309b49a09f3373f
FacundoJoaquinGil <facundojoagl@gmail.com>
2025-01-27 13:06:16 -0300

    Get all products printed

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ApiService } from '../../services/api.service';
+import { IProduct } from '../../models/product.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-products',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit{
+
+  pruductList: IProduct[] = [];
+ 
+
+  private _apiService = inject(ApiService);
+
+  ngOnInit(): void {
+  this._apiService.getAllProducts().subscribe((data: IProduct[]) => {
+    this.pruductList = data;
+    console.log(data)
+  })}
+
+  navegate(id: number){
+    console.log(id);
+  }
 
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { IProduct } from '../models/product.model';
 
 @Injectable({
   providedIn: 'root',
@@ -9,11 +10,11 @@ export class ApiService {
   private url: string = 'https://fakestoreapi.com/products';
   private _http = inject(HttpClient);
 
-  getAllProducts():Observable<any> {
-    return this._http.get(this.url);
+  getAllProducts():Observable<IProduct[]> {
+    return this._http.get<IProduct[]>(this.url);
   }
 
-  getProduct(id: number):Observable<any>{
-    return this._http.get(`${this.url}/${id}`);
+  getProduct(id: number):Observable<IProduct>{
+    return this._http.get<IProduct>(`${this.url}/${id}`);
   }
 }
