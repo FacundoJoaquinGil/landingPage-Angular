e0cc1c599bbed3bd1883b24afafea0d5e2e83bae
FacundoJoaquinGil <FacundoJoaGL@gmail.com>
2025-01-28 14:34:06 -0300

    get and injection angular 17

--- src/app/pages/product-detail/product-detail.component.ts
+++ src/app/pages/product-detail/product-detail.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { IProduct } from '../../models/product.model';
+import { ApiService } from '../../services/api.service';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-detail',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product-detail.component.html',
-  styleUrl: './product-detail.component.css'
+  styleUrl: './product-detail.component.css',
 })
 export class ProductDetailComponent {
+  
+  public product?: IProduct;
 
+  private _route = inject(ActivatedRoute);
+  private _apiService = inject(ApiService);
+
+  ngOnInit(): void {
+    this._route.params.subscribe((params) => {
+      this._apiService.getProduct(params['id']).subscribe((data: IProduct) => {
+        this.product = data;
+        console.log(data);
+      });
+    });
+
+  // ngOnInit(id:number): void {
+    
+  //   this._apiService.getProduct(id).subscribe((data: IProduct) => {
+  //       this.product = data;
+  //       console.log(data);
+  //     });
+  }
 }
--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -2,6 +2,7 @@ import { Component, inject, OnInit } from '@angular/core';
 import { ApiService } from '../../services/api.service';
 import { IProduct } from '../../models/product.model';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-products',
@@ -11,19 +12,20 @@ import { CommonModule } from '@angular/common';
 })
 export class ProductsComponent implements OnInit{
 
-  pruductList: IProduct[] = [];
- 
+  productList: IProduct[] = [];
 
+  private _route = inject(Router);
   private _apiService = inject(ApiService);
 
   ngOnInit(): void {
   this._apiService.getAllProducts().subscribe((data: IProduct[]) => {
-    this.pruductList = data;
+    this.productList = data;
     console.log(data)
   })}
 
   navegate(id: number){
-    console.log(id);
+    this._route.navigate(['/products', id]);
+    console.log(id);    
   }
 
 }
