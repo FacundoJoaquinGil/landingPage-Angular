9e5336d2e42ba3ec330d1c69579bddc349cb4a82
FacundoJoaquinGil <FacundoJoaGL@gmail.com>
2025-02-25 12:03:57 -0300

    add header, footer and navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,19 +3,16 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './pages/header/header.component';
 import { FooterComponent } from './pages/footer/footer.component';
+import { NavbarComponent } from './pages/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, CommonModule, HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, NavbarComponent, FooterComponent, RouterOutlet, RouterLink, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'landingPage';
-  menuOption: string = '';
-
-  onOption(menuOption: string) {
-    this.menuOption = menuOption;
-  }
+ 
 }
---
+++ src/app/pages/navbar/navbar.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [RouterOutlet, RouterLink, CommonModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+  menuOption: string = '';
+
+  onOption(menuOption: string) {
+    this.menuOption = menuOption;
+  }
+}
